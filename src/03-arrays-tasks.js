6d8d6e8ccc3140f3615f1b9a0b96433162e0d314
DenverDen <denverden@mail.ru>
2025-08-04 16:24:38 +0300

    feat: add solution getPositivesCount

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -303,8 +303,8 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.filter((value) => Number.isInteger(value) && value > 0).length;
 }
 
 /**
