6522306c9c66457414890b2a14e775a70508ede0
DenverDen <denverden@mail.ru>
2025-08-04 15:26:43 +0300

    feat: add solution isPrime

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -196,8 +196,15 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  const limit = Math.sqrt(n);
+
+  if (n <= 2) return true;
+  for (let i = 2; i <= limit; i += 1) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
 }
 
 /**
